b0c63f2c4a09773d9afaecffe13c429c71820931
mikk369 <mikk.ereline@vikk.ee>
2023-09-28 14:24:51 +0300

    As a user i can get list of  hours[#186106445]

--- controllers/hoursApi.js
+++ controllers/hoursApi.js
@@ -1,5 +1,23 @@
 const Hours = require('../models/hoursModel');
 
+exports.getAllhours = async (req, res, next) => {
+  try {
+    const hours = await Hours.find();
+    res.status(200).json({
+      status: 'success',
+      results: hours.length,
+      data: {
+        hours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 exports.createHours = async (req, res, next) => {
   try {
     const newHour = await Hours.create(req.body);
@@ -8,6 +26,19 @@ exports.createHours = async (req, res, next) => {
       hour: newHour,
     });
   } catch (error) {
-    res.status(400).json({ error: 'required fields are not filled or in invalid format' });
+    res.status(411).json({ error: 'One or more required fields empty' });
   }
+};
+
+exports.getHour = async (req, res, next) => {
+  const hours = await Hours.findById(req.params.id);
+  if (!hours) {
+    return next('hours with given ID not found', 404);
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      hours,
+    },
+  });
 };
--- routes/hoursRoutes.js
+++ routes/hoursRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const hoursApi = require('./../controllers/hoursApi');
 const authController = require('./../controllers/authController');
 
-router.route('/').post(authController.protect, hoursApi.createHours);
+router
+  .route('/')
+  .post(authController.protect, hoursApi.createHours)
+  .get(authController.protect, hoursApi.getAllhours);
 
 module.exports = router;
